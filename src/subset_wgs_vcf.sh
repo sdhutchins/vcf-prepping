c692ae52bef9cf7644ae29a3e302fd7c226148ae
Shaurita Hutchins <shaurita.d.hutchins@gmail.com>
2020-03-09 18:12:00 -0500

    Removed loading of modules.

--- src/subset_wgs_vcf.sh
+++ src/subset_wgs_vcf.sh
@@ -9,18 +9,13 @@
 #PBS -o logs/
 #PBS -e logs/
 
-EMAIL=shaurita.d.hutchins@gmail.com
-
 DATA_DIR=/ddn/home3/vallender/projects/NPRC_Pedigree_Project/data
 MGAPDIR=/ddn/home3/vallender/projects/NPRC_Pedigree_Project/data/mGAP
 INFILE=${MGAPDIR}/all_nprcs_rminfo_wgs.vcf.gz
 OUTFILE=${MGAPDIR}/all_nprcs_rminfo_wgs_subsetted.vcf.gz
 SAMPLES_FILE=${DATA_DIR}/sample_id_lists/exclude_ids_plink_pca.txt
 
-module use /ddn/home3/vallender/software/module_files
-module load nprc_project
-
-bcftools view $INFILE -S ^$SAMPLES_FILE -O z -o $OUTFILE -l 9 --threads 50
+bcftools view $INFILE -S ^$SAMPLES_FILE -O z -o $OUTFILE --threads 50
 
 bcftools index $OUTFILE --threads 50
 
